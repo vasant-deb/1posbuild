d33c78d6c6beab2c8445ad588b854e087b871331
vasant-deb <iamvasantsharma@gmail.com>
2023-10-30 14:45:49 -0400

    new

--- 1pos.sql
+++ 1pos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 30, 2023 at 03:22 AM
+-- Generation Time: Oct 30, 2023 at 05:54 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
